40b40d0dba640b81320a010d481a4307216da226
Zhang WenChao <zhangwenchao001@gmail.com>
2018-11-26 16:01:01 +0800

    Unix-like serialport test case
    Signed-off-by: Zhang WenChao <zhangwenchao001@gmail.com>

---
+++ test/serialport_uinttest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "serialport.h"
+}
+
+namespace {
+
+    char *tty_1_path = const_cast<char *>("/dev/tty.usbserial-144100");
+
+    struct sp_port port;
+
+    TEST(SerialPort, Open) {
+        iot_sp_init(&port, tty_1_path);
+    }
+}
